5f76395a796295a75908aef7c98e2c70da825c94
Jackson Bailey <Bailey95jackson@gmail.com>
2021-12-24 15:50:48 -0500

    Dev: Added a function that gets the current screen size and swaps out the card image based on the size

--- src/components/recipeCardTemplate.jsx
+++ src/components/recipeCardTemplate.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {NavLink} from 'react-router-dom'
 
 export default function RecipeCardTemplate({images, title, uuid, description, servings, cookTime, prepTime}) {
+    
+
+    //This assesses the current screen size, checks if the size has changed, and updates it if it has.
+    const [screenSize, getDimension] = useState({dynamicWidth:window.innerWidth, dynamicHeight: window.innerHeight});
+    
+    const setDimension = () => {
+        getDimension({
+            dynamicWidth: window.innerWidth,
+            dynamicHeight: window.innerHeight
+        })
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', setDimension);
+        return (() => {
+            window.removeEventListener('resize', setDimension)
+        })
+    }, [screenSize])
+
+
+
     return (
         <React.Fragment>
             <NavLink to={`/recipes/:${uuid}`} className='recipe-link' key={uuid}>
                 <article className='recipe-card' id={uuid}>
                     <figure className='recipe-img'>
-                        <img src={`http://localhost:3001${images.small}`} alt={`${title}`}/>
+                        <img src={`http://localhost:3001${(screenSize.dynamicWidth >= 993) ? images.medium : images.small}`} alt={`${title}`}/>
                     </figure>
 
                     <header className='recipe-content'>
